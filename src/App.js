348534d9ca6dc06fde33e86b459bd7a1deea4e07
jui-kumkum <jui.kumkum@gmail.com>
2018-12-26 13:27:41 +0600

    food rating

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import DefaultView from './DefaultView';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <DefaultView />
+       </div>
+    );
+  }
+}
+
+export default App;
